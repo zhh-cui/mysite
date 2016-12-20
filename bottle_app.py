b406f79d445764adbb8e4296357a6cf6e6da1441
CUI Zhaohui <zhh.cui@outlook.com>
2016-12-20 16:27:51 +0800

    将添加用户和查询记录页面，更换成首页上的按钮，简化操作。

--- bottle_app.py
+++ bottle_app.py
@@ -34,84 +34,65 @@ def hello(db):
 
 @bottle.post("/")
 def do_hello(db):
-    mobilephone = bottle.request.forms.get("mobilephone").strip()
-    if (len(mobilephone) != 11) or (not mobilephone.isdecimal()) or (mobilephone[0] != '1'):
-        return "输入的手机号：{}好像不正确，请重新输入。".format(mobilephone)
-    checkphone = db.execute("SELECT COUNT(*) FROM mobilephone_dict WHERE mobilephone = ?", (mobilephone,)).fetchone()
-    if 1 != checkphone[0]:
-        return "输入的手机号：{}好像还未注册过。".format(mobilephone)
+    if bottle.request.forms.get("save", "").strip():
+        mobilephone = bottle.request.forms.get("mobilephone").strip()
+        if (len(mobilephone) != 11) or (not mobilephone.isdecimal()) or (mobilephone[0] != '1'):
+            return "输入的手机号：{}好像不正确，请重新输入。".format(mobilephone)
+        checkphone = db.execute("SELECT COUNT(*) FROM mobilephone_dict WHERE mobilephone = ?", (mobilephone,)).fetchone()
+        if 1 != checkphone[0]:
+            return "输入的手机号：{}好像还未注册过。".format(mobilephone)
 
-    timestamp = bottle.request.forms.get("timestamp").strip()
-    try:
-        check = datetime.datetime.strptime(timestamp, "%Y-%m-%d").date()
-    except ValueError:
-        return "输入的日期：{}好像不正确。".format(timestamp)
+        timestamp = bottle.request.forms.get("timestamp").strip()
+        try:
+            check = datetime.datetime.strptime(timestamp, "%Y-%m-%d").date()
+        except ValueError:
+            return "输入的日期：{}好像不正确。".format(timestamp)
 
-    projectname = bottle.request.forms.getunicode("projectname").strip()
+        projectname = bottle.request.forms.getunicode("projectname").strip()
 
-    taskname = bottle.request.forms.getunicode("taskname").strip()
+        taskname = bottle.request.forms.getunicode("taskname").strip()
 
-    timeinhour = bottle.request.forms.get("timeinhour").strip()
-    try:
-        timeinhour = float(timeinhour)
-    except ValueError:
-        return "输入的耗费时间：{}好像不正确。".format(timeinhour)
-    if timeinhour % 0.5:
-        return "输入的耗费时间：{}好像不正确。".format(timeinhour)
+        timeinhour = bottle.request.forms.get("timeinhour").strip()
+        try:
+            timeinhour = float(timeinhour)
+        except ValueError:
+            return "输入的耗费时间：{}好像不正确。".format(timeinhour)
+        if timeinhour % 0.5:
+            return "输入的耗费时间：{}好像不正确。".format(timeinhour)
 
-    location = bottle.request.forms.getunicode("location").strip()
+        location = bottle.request.forms.getunicode("location").strip()
 
-    summary = bottle.request.forms.getunicode("summary").strip()
-    summary = summary.split('(')[0].strip()
+        summary = bottle.request.forms.getunicode("summary").strip()
+        summary = summary.split('(')[0].strip()
 
-    db.execute("INSERT INTO workingdiary (mobilephone, timestamp, projectname, taskname, timeinhour, location, summary) VALUES (?,?,?,?,?,?,?)", (mobilephone, timestamp, projectname, taskname, timeinhour, location, summary))
-    result = db.execute("SELECT MAX(id) FROM workingdiary where mobilephone = ?", (mobilephone,)).fetchone()
-    newid = result[0]
+        db.execute("INSERT INTO workingdiary (mobilephone, timestamp, projectname, taskname, timeinhour, location, summary) VALUES (?,?,?,?,?,?,?)", (mobilephone, timestamp, projectname, taskname, timeinhour, location, summary))
+        result = db.execute("SELECT MAX(id) FROM workingdiary where mobilephone = ?", (mobilephone,)).fetchone()
+        newid = result[0]
 
-    return "新纪录：{}创建成功。".format(newid)
+        return "新纪录：{}创建成功。".format(newid)
 
-@bottle.route("/add_user")
-def add_user():
-    return '''
-        <form action="/add_user" method="post">
-            手机号: <input name="mobilephone" type="text" />
-            <input value="提交" type="submit" />
-        </form>
-    '''
+    elif bottle.request.forms.get("query", "").strip():
+        mobilephone = bottle.request.forms.get("mobilephone").strip()
+        if (len(mobilephone) != 11) or (not mobilephone.isdecimal()) or (mobilephone[0] != '1'):
+            return "输入的手机号：{}好像不正确，请重新输入。".format(mobilephone)
+        checkphone = db.execute("SELECT COUNT(*) FROM mobilephone_dict WHERE mobilephone = ?", (mobilephone,)).fetchone()
+        if 1 != checkphone[0]:
+            return "输入的手机号：{}好像还未注册过。".format(mobilephone)
 
-@bottle.post("/add_user")
-def do_add_user(db):
-    newmobilephone = bottle.request.forms.get("mobilephone").strip()
-    checkphone = db.execute("SELECT COUNT(*) FROM mobilephone_dict WHERE mobilephone = ?", (newmobilephone,)).fetchone()
-    if 1 == checkphone[0]:
-        return "手机号：{}已经注册过。".format(newmobilephone)
+        result = db.execute("SELECT * FROM workingdiary WHERE mobilephone = ? order by date(timestamp) desc", (mobilephone,))
+        return bottle.template("show_record", records = result)
 
-    if (len(newmobilephone) != 11) or (not newmobilephone.isdecimal()) or (newmobilephone[0] != '1'):
-        return "输入的手机号：{}好像不正确，请重新输入。".format(newmobilephone)
+    elif bottle.request.forms.get("adduser", "").strip():
+        mobilephone = bottle.request.forms.get("mobilephone").strip()
+        checkphone = db.execute("SELECT COUNT(*) FROM mobilephone_dict WHERE mobilephone = ?", (mobilephone,)).fetchone()
+        if 1 == checkphone[0]:
+            return "手机号：{}已经注册过。".format(mobilephone)
 
-    db.execute("INSERT INTO mobilephone_dict (mobilephone) VALUES (?)", (newmobilephone,))
-    return "手机号：{}注册成功。".format(newmobilephone)
+        if (len(mobilephone) != 11) or (not mobilephone.isdecimal()) or (mobilephone[0] != '1'):
+            return "输入的手机号：{}好像不正确，请重新输入。".format(mobilephone)
 
-@bottle.route("/query")
-def query():
-    return '''
-        <form action="/query" method="post">
-            手机号: <input name="mobilephone" type="text" />
-            <input value="提交" type="submit" />
-        </form>
-    '''
-
-@bottle.post("/query")
-def do_query(db):
-    mobilephone = bottle.request.forms.get("mobilephone").strip()
-    if (len(mobilephone) != 11) or (not mobilephone.isdecimal()) or (mobilephone[0] != '1'):
-        return "输入的手机号：{}好像不正确，请重新输入。".format(mobilephone)
-    checkphone = db.execute("SELECT COUNT(*) FROM mobilephone_dict WHERE mobilephone = ?", (mobilephone,)).fetchone()
-    if 1 != checkphone[0]:
-        return "输入的手机号：{}好像还未注册过。".format(mobilephone)
-
-    result = db.execute("SELECT * FROM workingdiary WHERE mobilephone = ? order by date(timestamp) desc", (mobilephone,))
-    return bottle.template("show_record", records = result)
+        db.execute("INSERT INTO mobilephone_dict (mobilephone) VALUES (?)", (mobilephone,))
+        return "手机号：{}注册成功。".format(mobilephone)
 
 @bottle.route("/edit/<id:int>")
 def edit(id, db):
--- submit_record.tpl
+++ submit_record.tpl
@@ -2,15 +2,17 @@
 <table width="500">
 <tr>
     <td width="100">手机号：</td>
-    <td><input name="mobilephone" type="text" value="" style="width:400px;" /></td>
+    <td><input name="mobilephone" type="text" value="" style="width:250px;" /></td>
+    <td><input name="query" value="查询" type="submit" /></td>
+    <td><input disabled name="adduser" value="添加" type="submit" /></td>
 </tr>
 <tr>
     <td width="100">日期：</td>
-    <td><input name="timestamp" type="text" value="{{timestamp}}" style="width:400px;" /></td>
+    <td colspan=3><input name="timestamp" type="text" value="{{timestamp}}" style="width:400px;" /></td>
 </tr>
 <tr>
     <td width="100">现有项目：</td>
-    <td><table>
+    <td colspan=3><table>
 %for item in projects:
         <tr><td>{{item}}</td></tr>
 %end
@@ -18,19 +20,19 @@
 </tr>
 <tr>
     <td width="100">项目名称：</td>
-    <td><input name="projectname" type="text" value="" style="width:400px;" /></td>
+    <td colspan=3><input name="projectname" type="text" value="" style="width:400px;" /></td>
 </tr>
 <tr>
     <td width="100">任务名称：</td>
-    <td><input name="taskname" type="text" value="" style="width:400px;" /></td>
+    <td colspan=3><input name="taskname" type="text" value="" style="width:400px;" /></td>
 </tr>
 <tr>
     <td width="100">耗费时间：</td>
-    <td><input name="timeinhour" type="text" value="（以0.5小时为最小单位）" style="width:400px;" /></td>
+    <td colspan=3><input name="timeinhour" type="text" value="（以0.5小时为最小单位）" style="width:400px;" /></td>
 </tr>
 <tr>
     <td width="100">现有地点：</td>
-    <td><table>
+    <td colspan=3><table>
 %for item in locations:
         <tr><td>{{item}}</td></tr>
 %end
@@ -38,12 +40,12 @@
 </tr>
 <tr>
     <td width="100">地点：</td>
-    <td><input name="location" type="text" value="公司" style="width:400px;" /></td>
+    <td colspan=3><input name="location" type="text" value="公司" style="width:400px;" /></td>
 </tr>
 <tr>
     <td width="100">备注：</td>
-    <td><textarea name="summary" type="text" value="" style="width:400px;height:100px;">交通：，住宿：，餐饮：，其他：(这里列出的关键字和标点符号不要改动)</textarea></td>
+    <td colspan=3><textarea name="summary" type="text" value="" style="width:400px;height:100px;">交通：，住宿：，餐饮：，其他：(这里列出的关键字和标点符号不要改动)</textarea></td>
 </tr>
 </table>
-<input value="提交" type="submit" />
+<input name="save" value="提交" type="submit" />
 </form>
